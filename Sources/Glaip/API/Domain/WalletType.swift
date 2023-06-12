c962a6dc0d9281ef315965c36c618138bcc2faa2
Florin Pop <florin@ae.studio>
2023-06-12 14:44:26 +0200

    Add alpha wallet URI

--- Sources/Glaip/API/Domain/WalletType.swift
+++ Sources/Glaip/API/Domain/WalletType.swift
@@ -8,5 +8,6 @@
 public enum WalletType: String, Equatable {
   case MetaMask = "https://metamask.app.link/wc?uri="
   case Rainbow = "https://rnbwapp.com/wc?uri="
+  case AlphaWallet = "https://aw.app/wc?uri="
   case TrustWallet = "https://link.trustwallet.com/wc?uri="
 }
